ee020ab03a7ad66e72aabae2d0a7d6f048e4d65a
Ivo <5323639+ivonoppen@users.noreply.github.com>
2025-07-20 15:56:26 +0200

    Dirty fix

--- custom_components/alfen_modbus/number.py
+++ custom_components/alfen_modbus/number.py
@@ -126,8 +126,9 @@ class AlfenNumber(NumberEntity):
     def update_value(self):
         builder = BinaryPayloadBuilder(byteorder=Endian.BIG, wordorder=Endian.BIG)
         value = self._hub.data[self._key]
-        if "socket_"+str(self._socket)+"_actualMaxCurrent" in self._hub.data:
-            self._attr_native_max_value = self._hub.data["socket_"+str(self._socket)+"_actualMaxCurrent"]
+        #if "socket_"+str(self._socket)+"_actualMaxCurrent" in self._hub.data:
+        #    self._attr_native_max_value = self._hub.data["socket_"+str(self._socket)+"_actualMaxCurrent"]
+        self._attr_native_max_value = 16 
         _LOGGER.debug("Updating value to: %f",value)
         if self._fmt == "u":
             builder.add_16bit_uint(int(value))
